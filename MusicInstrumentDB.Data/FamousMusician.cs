b5f812629777ef141c969c33da776c1a31ca61a6
JoelS-1 <82602257+JoelS-1@users.noreply.github.com>
2021-06-07 16:05:51 -0400

    added famous musician data

---
+++ MusicInstrumentDB.Data/FamousMusician.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicInstrumentDB.Data
+{
+    public class FamousMusician
+    {
+        [Key]
+        public int FamousMusicianId { get; set; }
+
+        [Required]
+        public int InstrumentId { get; set; }
+        [ForeignKey(nameof(InstrumentId))]
+        public virtual List<Instrument> Instruments { get; set; } = new List<Instrument>();
+
+        [Required]
+        public int MusicGenre { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public Guid OwnderId { get; set; }
+    }
+}
