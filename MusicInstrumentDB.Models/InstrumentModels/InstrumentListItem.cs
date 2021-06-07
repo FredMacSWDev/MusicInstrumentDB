bc9c6bfb730a2d393ba11c505a879b5e982cee75
Luis Costilla <luis287@gmail.com>
2021-06-07 16:50:00 -0400

    InstrumentListItem added to Instrument Models

---
+++ MusicInstrumentDB.Models/InstrumentModels/InstrumentListItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicInstrumentDB.Models.InstrumentModels
+{
+    public class InstrumentListItem
+    {
+        public int InstrumentId { get; set; }
+        public string InstrumentName { get; set; }
+
+        public int? FamilyId { get; set; }
+        public string InstrumentFamilyName { get; set; }
+    }
+}
