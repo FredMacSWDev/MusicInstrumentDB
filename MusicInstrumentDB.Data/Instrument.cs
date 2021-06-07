ab917628d075339a51ab21a268f2d8675024470c
Luis Costilla <luis287@gmail.com>
2021-06-07 16:00:44 -0400

    Instrument Class added in Data layer

---
+++ MusicInstrumentDB.Data/Instrument.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicInstrumentDB.Data
+{
+    public class Instrument
+    {
+        [Key]
+        public int InstrumentId { get; set; }
+        [Required]
+        public string InstrumentName { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public string Transposition { get; set; }
+
+
+        [ForeignKey(nameof(InstrumentFamily))]
+        public int FamilyId { get; set; }
+        public virtual InstrumentFamily InstrumentFamily { get; set; }
+
+
+        public virtual List<FamousMusician> FamousMusicians { get; set; } = new List<FamousMusician>();
+    }
+}
